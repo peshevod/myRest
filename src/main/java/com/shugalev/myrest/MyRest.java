7462a25a79e31b0181374a0350d35559608f0004
Ilya Shugalev <ilya@shugalev.com>
2022-07-23 11:21:06 +0300

    MyLogger Test

--- src/main/java/com/shugalev/myrest/MyRest.java
+++ src/main/java/com/shugalev/myrest/MyRest.java
@@ -9,10 +9,6 @@ package com.shugalev.myrest;
     Main Program
 */
 import org.apache.camel.CamelContext;
-import org.apache.camel.TypeConverter;
-import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.spi.BeanRepository;
-import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/shugalev/myrest/MyLoggerTest.java
+++ src/test/java/com/shugalev/myrest/MyLoggerTest.java
@@ -1,27 +1,32 @@
 package com.shugalev.myrest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Suite
 class MyLoggerTest {
     @Autowired
     private MyLogger myLogger;
     @Test
-    void getLogger() {
-        if(myLogger==null) System.out.println("logger=null");
-    }
-
-    @Test
-    void getLogger1() {
-    }
-
-    @Test
-    void getMyLogger() {
-    }
-
-    @Test
-    void setMyLogger() {
+    void configureMyLogger() {
+        assertNotNull(myLogger, "myLogger=null");
+        assertNotNull(myLogger.getLogger(),"getLogger() is null");
+        assertNotNull(myLogger.getLogger1(),"getLogger1() is null");
+        assertNotNull(myLogger.getMyLogger(),"Map is null");
+        assertNotNull(((org.apache.logging.log4j.core.Logger)(myLogger.getLogger())).getAppenders(),"logger No appenders");
+        assertNotNull(((org.apache.logging.log4j.core.Logger)(myLogger.getLogger1())).getAppenders(),"logger1 No appenders");
+        assertNotEquals(((org.apache.logging.log4j.core.Logger)(myLogger.getLogger())).getAppenders().size(),0);
+        assertNotEquals(((org.apache.logging.log4j.core.Logger)(myLogger.getLogger1())).getAppenders().size(),0);
+        assertNotNull(((org.apache.logging.log4j.core.Logger)(myLogger.getLogger())).getAppenders().get("Console"), "logger No console appender");
+        assertNotNull(((org.apache.logging.log4j.core.Logger)(myLogger.getLogger())).getAppenders().get("ApplicationFileLog"), "logger No File appender");
+        assertNotNull(((org.apache.logging.log4j.core.Logger)(myLogger.getLogger1())).getAppenders().get("CONSOLE_ERR"), "logger1 No console appender");
+        assertNotNull(((org.apache.logging.log4j.core.Logger)(myLogger.getLogger1())).getAppenders().get("FILE_ERR"), "logger1 No File appender");
     }
 }
